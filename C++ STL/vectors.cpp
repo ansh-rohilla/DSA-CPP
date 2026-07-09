0a16c3a79b33870093e01420335a888c08d2ab15
Ansh Rohilla <anshrohilla.b81@gmail.com>
2026-07-09 19:33:36 +0530

    Update vector STL examples

--- C++ STL/vectors.cpp
+++ C++ STL/vectors.cpp
@@ -9,10 +9,21 @@ int main(){
     for(auto it=vec.begin() ; it!=vec.end() ; it++){
         cout<<*(it)<<endl;                
     }
+    cout<<"-----------------"<<endl;
     // vector<int>:: reverse_iterator itr;
     for(auto itr=vec.rbegin() ; itr!=vec.rend() ; itr++){
         cout<<*(itr)<<endl;
     }
     
+
+    // vector<int> vec2 = {10,20,30,40,50};
+    // // vec2.erase(vec2.begin()+1,vec2.begin()+3); // erase 20 and 30 start included but not the end
+    // vec2.insert(vec2.begin()+2,100); // insert 100 at index 1
+
+    // for(auto it=vec2.begin() ; it!=vec2.end() ; it++){
+    //     cout<<*(it)<<endl;                
+    // }
+
+
     return 0;
 }
