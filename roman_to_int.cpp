54b50c8ac83f216f16cfdbecd3b34ae86b92db52
Ansh Rohilla <anshrohilla.b81@gmail.com>
2026-07-16 22:20:29 +0530

    Add Roman numeral conversion solution

---
+++ roman_to_int.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+
+        unordered_map<char, int> mp;
+
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // If current value is smaller than the next value,
+            // subtract it instead of adding.
+            if (i + 1 < s.length() && mp[s[i]] < mp[s[i + 1]]) {
+                ans -= mp[s[i]];
+            }
+            else {
+                ans += mp[s[i]];
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    string s;
+    cin >> s;
+
+    cout << obj.romanToInt(s) << endl;
+
+    return 0;
+}
